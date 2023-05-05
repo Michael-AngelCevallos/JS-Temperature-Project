e5135eec6c47b24dafe6b4023d819c4a74394422
Michael-Angel Cevallos <michael.a.cevallos@gmail.com>
2023-05-04 21:51:59 -0500

    added celsius

--- js/temp.js
+++ js/temp.js
@@ -9,3 +9,7 @@ console.log(fahrenheit);
 // get rid of decimal
 fahrenheit = Math.floor(fahrenheit);
 console.log(fahrenheit);
+console.log(`The temperature is ${fahrenheit} degrees Fahrenheit.`)
+const newton = celsius * (33/100);
+console.log(Math.floor(newton));
+console.log(`The temperature in Newton is ${Math.floor(newton)}.`)
