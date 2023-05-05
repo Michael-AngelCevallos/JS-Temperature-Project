5a44988f2bb9bd616b768cd894c0df46c829246a
Michael-Angel Cevallos <michael.a.cevallos@gmail.com>
2023-05-04 21:50:21 -0500

    added fahrenheit and a Math.floor method

--- js/temp.js
+++ js/temp.js
@@ -3,3 +3,9 @@ const kelvin = 0;
 // converts kelvin to celsius
 const celsius = kelvin - 273;
 console.log(celsius);
+// convert celsius to fahrenheit
+let fahrenheit = celsius * (9/5) + 32;
+console.log(fahrenheit);
+// get rid of decimal
+fahrenheit = Math.floor(fahrenheit);
+console.log(fahrenheit);
