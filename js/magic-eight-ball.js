dc1bba661850bb983c25e5ea96ed573308c31942
Michael-Angel Cevallos <michael.a.cevallos@gmail.com>
2023-05-06 14:07:43 -0500

    finished if/else statementy but still nees to add alot more and fix

--- js/magic-eight-ball.js
+++ js/magic-eight-ball.js
@@ -23,9 +23,32 @@
 let userName = '';
 let userQuestion = 'Magic Eight Ball?! What Does My Future Hold For Me?!';
 let randomNumber = Math.floor(Math.random()) * 8;
+let eightBall = '';
+
+// userName ? console.log(`Hello, ${userName}!`) : console.log('Hello!');
 
 
-userName ? console.log(`Hello, ${userName}!`) : console.log('Hello!');
+if(randomNumber === 0){
+    console.log(eightBall);
+}else if(randomNumber === 1){
+    console.log('It is decidedly so');
+} else if(randomNumber === 2){
+    console.log('Reply hazy try again');
+} else if(randomNumber === 3) {
+    console.log('Cannot predict now');
+}else if(randomNumber === 4){
+    console.log('Do not count on it');
+}else if(randomNumber === 5){
+    console.log('My sources say no');
+}else if(randomNumber === 6){
+    console.log('Outlook not so good');
+}else if(randomNumber === 7){
+    console.log('Signs point to yes');
+} else {
+    console.log('Go away');
+}
 
-console.log(userQuestion);
-console.log(randomNumber);
+// console.log(userQuestion);
+// console.log(randomNumber);
+
+console.log(eightBall);
