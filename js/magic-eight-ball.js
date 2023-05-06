bbf45e8f8d39b9a2659d0c5aab9cf33193a609e9
Michael-Angel Cevallos <michael.a.cevallos@gmail.com>
2023-05-06 13:49:18 -0500

    Added user question and tenary concept for username

--- js/magic-eight-ball.js
+++ js/magic-eight-ball.js
@@ -6,8 +6,16 @@
 //     The user will be able to input a question, then our program will output a random fortune. */
 
 
-/*In the first line of the program, define a variable called userName that is set to an empty string.
+/* #1 In the first line of the program, define a variable called userName that is set to an empty string.
 
     If the user wants, they can enter their name in between the quotation marks.*/
 
+/* #2 Below this variable, create a ternary expression that decides what to do if the user enters a name or not. If the user enters a name — like 'Jane' — use string interpolation to log Hello, Jane! to the console. Otherwise, simply log Hello!.*/
+
+/* #3
+Create a variable named userQuestion. The value of the variable should be a string that is the question the user wants to ask the Magic Eight Ball.*/
+
 let userName = '';
+let userQuestion = 'Magic Eight Ball?! What Does My Future Hold For Me?!';
+userName ? console.log(`Hello, ${userName}!`) : console.log('Hello!');
+
