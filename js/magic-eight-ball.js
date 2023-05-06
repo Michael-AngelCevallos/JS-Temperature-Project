fc49c5dbe144765678a9f9989f7ee8b03651dbdf
Michael-Angel Cevallos <michael.a.cevallos@gmail.com>
2023-05-06 13:41:49 -0500

    started the Magic eight ball project

---
+++ js/magic-eight-ball.js
@@ -0,0 +1,13 @@
+/** Magic Eight Ball
+// You’ve learned a powerful tool in JavaScript: control flow! It’s so powerful, in fact, that it can be used to tell someone’s fortune.
+//
+//     In this project we will build a Magic Eight Ball using control flow in JavaScript.
+//
+//     The user will be able to input a question, then our program will output a random fortune. */
+
+
+/*In the first line of the program, define a variable called userName that is set to an empty string.
+
+    If the user wants, they can enter their name in between the quotation marks.*/
+
+let userName = '';
