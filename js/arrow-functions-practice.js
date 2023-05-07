4e9a4537c769806881e686e698f128f2bae23b2b
Michael-Angel Cevallos <michael.a.cevallos@gmail.com>
2023-05-07 15:52:41 -0500

    finished exercise

--- js/arrow-functions-practice.js
+++ js/arrow-functions-practice.js
@@ -1,12 +1,23 @@
 
 
 // Original Code
-const plantNeedsWater = function(day) {
+const plantNeedsWater = function(day = 'Wednesday') {
     if (day === 'Wednesday') {
         return true;
     } else {
         return false;
     }
 };
+console.log(plantNeedsWater())
 
 /** change above to use Arrow function */
+
+const plantNeedsWater2 = (day = 'Wednesday') => {
+    if (day === 'Wednesday') {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+console.log(plantNeedsWater2())
