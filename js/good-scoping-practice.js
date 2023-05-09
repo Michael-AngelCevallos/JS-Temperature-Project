c83ab275eb8add59515bd0d958e34b9c7733ac20
Michael-Angel Cevallos <michael.a.cevallos@gmail.com>
2023-05-08 22:06:51 -0500

    added lightWaves variable to if statement, watching diffrence

--- js/good-scoping-practice.js
+++ js/good-scoping-practice.js
@@ -1,12 +1,33 @@
 /* #1.Inside the function body of logVisibleLightWaves(), beneath the region variable and before the provided console.log() statement, create an if statement that checks if the region is the 'The Arctic'.*/
 
 
+// const logVisibleLightWaves = () => {
+//     let lightWaves = 'Moonlight';
+//     let region = 'The Arctic';
+//     if(region === 'The Arctic'){
+//         console.log(region);
+//     }
+//     console.log(lightWaves);
+// }
+//
+// logVisibleLightWaves();
+
+
+
+/* #2.Inside the if block, define a new let variable lightWaves and set it equal to 'Northern Lights'.*/
+
+ /* #3 Beneath the variable in the if block, use console.log() to log the value of the block variable inside the if block.
+Run your code and notice the output. Inside the if block console.log(lightWaves) logs the value Northern Lights to the console. Outside the if block, but still within the function, the same statement logs Moonlight to the console.*/
+
+
 const logVisibleLightWaves = () => {
     let lightWaves = 'Moonlight';
     let region = 'The Arctic';
     if(region === 'The Arctic'){
+        let lightWaves = 'Northern Lights';
         console.log(region);
-    } else {
-
     }
+    console.log(lightWaves);
 }
+
+logVisibleLightWaves();
