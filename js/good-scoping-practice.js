92f585b88fc0de01e27128c70ca948306a4934f1
Michael-Angel Cevallos <michael.a.cevallos@gmail.com>
2023-05-08 22:02:03 -0500

    Added function with if statment

---
+++ js/good-scoping-practice.js
@@ -0,0 +1,12 @@
+/* #1.Inside the function body of logVisibleLightWaves(), beneath the region variable and before the provided console.log() statement, create an if statement that checks if the region is the 'The Arctic'.*/
+
+
+const logVisibleLightWaves = () => {
+    let lightWaves = 'Moonlight';
+    let region = 'The Arctic';
+    if(region === 'The Arctic'){
+        console.log(region);
+    } else {
+
+    }
+}
