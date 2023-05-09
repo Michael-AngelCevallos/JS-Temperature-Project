dd6ad506d8a0ee14190585105d8fcf76fa3016fc
Michael-Angel Cevallos <michael.a.cevallos@gmail.com>
2023-05-08 22:25:59 -0500

    access array elements through their index

--- js/array-practice.js
+++ js/array-practice.js
@@ -5,3 +5,11 @@
 const hobbies = ['Basketball', ' Working out', ' Family Time.']
 
 console.log('My hobbies include: ' + hobbies); // prints the brackets and everything between
+
+
+// ACCESSING ELEMENTS
+
+const famousSayings = ['Fortune favors the brave.', 'A joke is a very serious thing.', 'Where there is love there is life.'];
+
+let listItem = famousSayings[0];
+console.log(listItem);
