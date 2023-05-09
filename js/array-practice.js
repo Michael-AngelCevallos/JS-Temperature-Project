3f3fcea9b959166e5373ed74f551360baa4691c3
Michael-Angel Cevallos <michael.a.cevallos@gmail.com>
2023-05-08 22:20:48 -0500

    MAde hobbies varaible, testing

--- js/array-practice.js
+++ js/array-practice.js
@@ -2,3 +2,6 @@
 
 //Creating an Array( which is a list of variables, numbers, boolean values)
 
+const hobbies = ['My Name ', 'is Mike, ', 'Cevallos.']
+
+console.log(hobbies); // prints the brackets and everything between
